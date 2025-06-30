422446e069ff2b8141efb5d8ea1716471c8888b4
TTrung <vothanhtrung9379@gmail.com>
2025-06-30 13:03:28 +0700

    update .env + utils/db.js

--- Backend/utils/db.js
+++ Backend/utils/db.js
@@ -1,9 +1,16 @@
+// File: Backend/utils/db.js
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('bloghub_db', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false // Bỏ nếu bạn muốn log SQL query
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mysql'
+  }
+);
 
 module.exports = sequelize;
